01d7c955ecd988c92791dceee857db0afecb0c02
yskshimizurowing-dev <ysk.shimizu.rowing@gmail.com>
2026-06-27 22:31:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ with col1:
         st.toast("トレーニングメニューは現在開発中です！")
 with col2:
     if st.button("📋\n\nホワイトボード", use_container_width=True):
-        st.switch_page("pages/02_Whiteboard.py")
+        st.switch_page("pages/whiteboard.py")
 with col3:
     # 欠席連絡（URLが用意できたら書き換えてください）
     st.link_button("📝\n\n欠席連絡", "https://docs.google.com/forms/d/e/1FAIpQLSdZWWNO0GpPOpadbspDl_YPgA_jx1Q2i4xUUdH44IinvhGY_w/viewform?usp=dialog", use_container_width=True)
