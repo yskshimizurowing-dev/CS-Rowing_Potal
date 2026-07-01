d886c5ba184756d130986e18de06cd0759a21946
yskshimizurowing-dev <ysk.shimizu.rowing@gmail.com>
2026-07-01 22:40:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ from utils import get_url
 
 st.set_page_config(page_title="ボート部専用ポータル", layout="centered", initial_sidebar_state="collapsed")
 
+# スタイル設定
 st.markdown('''
 <style>
     header[data-testid="stHeader"] { display: none !important; }
@@ -24,12 +25,11 @@ if st.user is not None:
                 item = visible_items[i + j]
                 with col:
                     url = get_url(item)
-                    # 画像をクリックできるようにリンクを貼る
+                    # HTMLタグをインデントなしで記述
                     st.markdown(f'<a href="{url}" target="_blank">', unsafe_allow_html=True)
                     st.image(item["icon"], use_container_width=True)
                     st.markdown('</a>', unsafe_allow_html=True)
                     
-                    # ボタンの描画
                     if item["type"] == "page":
                         if st.button(item["label"], use_container_width=True, key=f"btn_{i+j}"): 
                             st.switch_page(item["url"])
