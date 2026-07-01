b9285d9ae902a08119b96284f10688818f08cf12
yskshimizurowing-dev <ysk.shimizu.rowing@gmail.com>
2026-07-01 22:47:24 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ MENU_ITEMS = [
     {"label": "トレーニング\nメニュー", "key": "URL_TRAINING", "type": "gas", "icon": "assets/training.png", "visible": True},
     {"label": "乗艇練習\nホワイトボード", "key": "URL_WHITEBOARD", "type": "gas", "icon": "assets/whiteboard.png", "visible": True},
     {"label": "予定表", "url": "pages/schedule.py", "type": "page", "icon": "assets/schedule.png", "visible": True},
-    {"label": "欠席連絡\nフォーム", "key": "URL_FORM", "type": "link", "icon": "assets/form.png", "visible": True},
+    {"label": "欠席連絡\nフォーム", "key": "URL_FORM", "type": "link", "icon": "images/googleforms.jpg", "visible": True},
     {"label": "Average\n計算ツール", "url": "pages/calculator.py", "type": "page", "icon": "assets/calculator.png", "visible": True},
     {"label": "エルゴ測定記録\nデータベース", "key": "URL_ErgDB", "type": "gas", "icon": "assets/db.png", "visible": True},
     {"label": "リギング\nサポート", "key": "URL_RIGGING", "type": "gas", "icon": "assets/rigging.png", "visible": True},
